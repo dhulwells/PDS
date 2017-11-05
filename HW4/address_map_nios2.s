2d3bb9b1802379218c3667a12a458708d6042751
Sam Cuthbertson <samuel.cuthbertson@colorado.edu>
2017-11-05 14:24:30 -0700

    Finished HW4

--- HW4/address_map_nios2.s
+++ HW4/address_map_nios2.s
@@ -25,7 +25,7 @@
 # So these two frame buffers together consume 2,457,600 bytes, leaving
 # aproximately 13.5 Mbytes for the stack.
 # My Qsys configuration put these frame buffers in the 4th 16MB chunk of
-# DRAM. 
+# DRAM.
 .equ VGA_BUFFER_WIDTH,		640
 .equ VGA_BUFFER_HEIGHT,		480
 .equ VGA_PIXEL_DEPTH,		4			# 4-bytes per pixel
@@ -37,7 +37,7 @@
 .equ VGA_BUFFER_END,		(2 * VGA_BUFFER_SIZE) - 4  # 0x03257FFC
 .equ VGA_RED_PIXEL,			0x3FF00000 # a red pixel
 .equ VGA_GREEN_PIXEL,		0x000FFC00 # a green pixel
-.equ VGA_BLUE_PIXEL,		0x000003FF # a blue pixel 
+.equ VGA_BLUE_PIXEL,		0x000003FF # a blue pixel
 
 
 .equ ONCHIP_SRAM_BASE,     	0x08000000 	# 16 Kbytes
@@ -58,13 +58,13 @@
 #   Sector erase
 #   Page erase
 #
-#                      Byte addresses from Qsys      Word/32-bit address 
+#                      Byte addresses from Qsys      Word/32-bit address
 #                      -------------------------------------------------------------------------
-#   UFM, R/W, Sector 1 = 0x00_0000 to 0x00_7FFF : 0x00_0000 to 0x00_1FFC - 32Kbytes, 4 pages 
+#   UFM, R/W, Sector 1 = 0x00_0000 to 0x00_7FFF : 0x00_0000 to 0x00_1FFC - 32Kbytes, 4 pages
 #   UFM, R/W, Sector 2 = 0x00_8000 to 0x00_FFFF : 0x00_2000 to 0x00_3FFC - 32Kbytes, 4 pages
-#   UFM, R/W, Sector 3 = 0x01_0000 to 0x06_FFFF : 0x00_4000 ro 0x01_BFFC - 384Kbytes, 48 pages 
+#   UFM, R/W, Sector 3 = 0x01_0000 to 0x06_FFFF : 0x00_4000 ro 0x01_BFFC - 384Kbytes, 48 pages
 #   CFM, R/W, Sector 4 = 0x07_0000 to 0x0B_7FFF : 0x01_C000 to 0x02_DFFC
-#   CFM, R/W, Sector 5 = 0x0B_8000 to 0x15_FFFF : 0x02_E000 to 0x05_7FFC 
+#   CFM, R/W, Sector 5 = 0x0B_8000 to 0x15_FFFF : 0x02_E000 to 0x05_7FFC
 .equ ONCHIP_FLASH_BASE, 		0x09000000
 .equ ONCHIP_FLASH_SECTOR1_BASE,	0x09000000
 .equ ONCHIP_FLASH_SECTOR2_BASE,	0x09002000
@@ -72,55 +72,55 @@
 .equ ONCHIP_FLASH_END,     		0x0915FFFC
 .equ ONCHIP_FLASH_PAGE_SIZE,	8192		# bytes
 
-.equ ONCHIP_FLASH_CSR_BASE,		0x0a000000  # control and status registers for flash 
+.equ ONCHIP_FLASH_CSR_BASE,		0x0a000000  # control and status registers for flash
 .equ ONCHIP_FLASH_CSR_STATUS,   0x0a000000
 # resets to 0xFFFF_FC00
 #  [1:0] = 00 Idle
 #        = 01 Busy erase
-#        = 10 Busy program/write 
+#        = 10 Busy program/write
 #        = 11 Busy read
 #    [2] = 1 read sucessful
 #    [3] = 1 write sucessful
 #    [4] = 1 erase sucessful
 #    [9:5] = RO, sector 5,4,3,2,1; =1 sector protection
-#  [31:10] = pad = 1's 
-.equ ONCHIP_FLASH_CSR_CONTROL,  0x0a000004 
+#  [31:10] = pad = 1's
+.equ ONCHIP_FLASH_CSR_CONTROL,  0x0a000004
 # resets to 3FFF_FFFF
-#   [19:0] = page erase address 
+#   [19:0] = page erase address
 #  [22:20] = sector erase address; 001 thru 101
 #  [27:23] = sector 5,4,3,2,1; =1 write protection
-#  [31:28] = pad = 0011, 
+#  [31:28] = pad = 0011,
 
 # Basic DMA Qsys configuration:
-#   Length register = 14-bits, max value 2^14 - 1 = 16,383 bytes 
+#   Length register = 14-bits, max value 2^14 - 1 = 16,383 bytes
 #   Burst transfers = disabled
 #   Soft reset enabled = true
 #   Fifo depth = 16
 #   Use registers for FIFO implementation = false
 .equ DMA_BASE,              0x0b000000 # DMA controller
 .equ DMA_STATUS,			0x0b000000
-#    [0] = Done, =1, write 0 to status reg to clear Done bit 
+#    [0] = Done, =1, write 0 to status reg to clear Done bit
 #    [1] = Busy
-#    [2] = REOP, =1 read end-of-packet 
-#    [3] = WEOP, =1 write end-of-packet 
-#    [4] = LEN,  =1 when length register decrements to 0 
+#    [2] = REOP, =1 read end-of-packet
+#    [3] = WEOP, =1 write end-of-packet
+#    [4] = LEN,  =1 when length register decrements to 0
 .equ DMA_MSTR_READ_START,	0x0b000004
 .equ DMA_MSTR_WRITE_START,	0x0b000008
-.equ DMA_LENGTH,			0x0b00000C # length in bytes, 14-bits, max value = 16,383 bytes 
+.equ DMA_LENGTH,			0x0b00000C # length in bytes, 14-bits, max value = 16,383 bytes
 .equ DMA_CONTROL,			0x0b000018
 #    [0] = Byte
 #    [1] = Half-word
 #    [2] = Word
-#    [3] = Go    start DMA operation 
-#    [4] = I_EN  generates IRQ when Done bit is set 
+#    [3] = Go    start DMA operation
+#    [4] = I_EN  generates IRQ when Done bit is set
 #    [5] = REEN  ends transcation when read master is done
 #    [6] = WEEN  ends transaction when write master is done
 #    [7] = LEEN  ends transaction when length reg = 0
 #    [8] = RCON  holds read address constant
-#    [9] = WCON  holds write address constant 
+#    [9] = WCON  holds write address constant
 #   [10] = Double word
 #   [11] = Quad word
-#   [12] = SOFTWARERESET, write twice to 1 to reset 
+#   [12] = SOFTWARERESET, write twice to 1 to reset
 
 
 .equ NIOS2_DEBUG_MEM_SLAVE,	0x0e000000
@@ -150,15 +150,15 @@
 
 # Keys config:
 #   Edge capture = true, synchronous, rising, enable bit-clearing
-#   IRQ type - Edge (when any edge bit is set)  
+#   IRQ type - Edge (when any edge bit is set)
 .equ KEYS_IN_BASE,        	0x100000e0 # 2 push buttons, data in [1:0]
 .equ KEYS_IN_IRQ_MASK,		0x100000e8 # [1:0]
 .equ KEYS_IN_EDGE,			0x100000eC # [1:0] - Write 1 to clear (W1C)
 
-.equ ACCEL_SPI_BASE,      	0x10000100  # SPI accelerometer access registers 
+.equ ACCEL_SPI_BASE,      	0x10000100  # SPI accelerometer access registers
 										# [15:8] r/w data
 										#  [7:0] w   address
-										
+
 # Accelerometer register addresses
 .equ ADXL345_INT_SOURCE,	0x30		# Interrupt source register, clear IRQ by reading this register
 
@@ -171,47 +171,42 @@
 .equ ADXL345_DATAZL,		0x36		# Z  [7:0], 2's complement value
 .equ ADXL345_DATAZH,		0x37		# Z [12:8]
 
-# From the auto-init hardware built by Qsys in Quartus 
+# From the auto-init hardware built by Qsys in Quartus
 #	case (rom_address)        addr  value
-#	0		:	data	<=	{6'h24, 8'h20}; // 8-bits, unsigned, threshold activity    = 32 * 62.5 mg = 2000 mg 
+#	0		:	data	<=	{6'h24, 8'h20}; // 8-bits, unsigned, threshold activity    = 32 * 62.5 mg = 2000 mg
 #	1		:	data	<=	{6'h25, 8'h03}; // 8-bits, unsigned, threshold inactivity  =  3 * 62.5 mg = 187.5 mg
-#	2		:	data	<=	{6'h26, 8'h01}; // 8-bits, unsigned, time inactivity       =  1 * 1 sec   = 1 sec 
+#	2		:	data	<=	{6'h26, 8'h01}; // 8-bits, unsigned, time inactivity       =  1 * 1 sec   = 1 sec
 #	3		:	data	<=	{6'h27, 8'h7f}; // 8-bits, Activity/inactivity control/detection
 #                                              Act DC coupled,     ACT x,y,z = 1
 #                                              Inact AC coupled, INACT x,y,z = 1
-#	4		:	data	<=	{6'h28, 8'h09}; // 8-bits, unsigned, Threshold Free-fall   = 9 * 62.5 mg  = 562.5 mg 
-#	5		:	data	<=	{6'h29, 8'h46}; // 8-bits, Time free-fall                  = 70 * 5 ms    = 350 ms 
-#	6		:	data	<=	{6'h2c, 8'h09}; // bandwidth rate, data rate and power mode control 
-#                                           //   low-power [4] = 0, rate = 9 -> output data rate : 50 Hz  
+#	4		:	data	<=	{6'h28, 8'h09}; // 8-bits, unsigned, Threshold Free-fall   = 9 * 62.5 mg  = 562.5 mg
+#	5		:	data	<=	{6'h29, 8'h46}; // 8-bits, Time free-fall                  = 70 * 5 ms    = 350 ms
+#	6		:	data	<=	{6'h2c, 8'h09}; // bandwidth rate, data rate and power mode control
+#                                           //   low-power [4] = 0, rate = 9 -> output data rate : 50 Hz
 #	7		:	data	<=	{6'h2E, 8'h10}; // Interrupt enable, Activity [4] = 1
-#	8		:	data	<=	{6'h2F, 8'h10}; // Interrupt map, 0's set INT1 pin, 1's set INT2 pin, Activity [4] -> INT2 
-#	9		:	data	<=	{6'h31, 8'h40}; // Data format SPI [6] = 1 -> 3 wire SPI mode; Int Invert [5] = IRQ signal is =1 
-#	10		:	data	<=	{6'h2d, 8'h08}; // Power control, power saving, measure [3] = 1, puts part into measurement mode 
+#	8		:	data	<=	{6'h2F, 8'h10}; // Interrupt map, 0's set INT1 pin, 1's set INT2 pin, Activity [4] -> INT2
+#	9		:	data	<=	{6'h31, 8'h40}; // Data format SPI [6] = 1 -> 3 wire SPI mode; Int Invert [5] = IRQ signal is =1
+#	10		:	data	<=	{6'h2d, 8'h08}; // Power control, power saving, measure [3] = 1, puts part into measurement mode
 #	default	:	data	<=	14'h0000;
 #	endcase
 
 # VGA controller
-.equ VGA_BASE,              0x10000120  # VGA controller registers 
-.equ VGA_BUFFER,     		0x10000120  # Read-only, Buffer start address 
-.equ VGA_BACKBUFFER,        0x10000124  # Read/Write, Back buffer start address 
+.equ VGA_BASE,              0x10000120  # VGA controller registers
+.equ VGA_BUFFER,     		0x10000120  # Read-only, Buffer start address
+.equ VGA_BACKBUFFER,        0x10000124  # Read/Write, Back buffer start address
 .equ VGA_RESOLUTION,        0x10000128  # Read-only, [31:16]=Y, [15:0]=X
-.equ VGA_STATUS,            0x1000012C  # Read-only, 
-										# [31:24] = m, width of Y coordinate address 
+.equ VGA_STATUS,            0x1000012C  # Read-only,
+										# [31:24] = m, width of Y coordinate address
 										# [23:16] = n, width of X coordinate address
 										#   [7:4] = B, number of bytes per pixel
 										#     [1] = A, addressing mode, (X-Y or consecutive)
 										#     [0] = S, swap
 
 # ---------------------------------------------------
-# Interrupt enable/mask bits for CPU ienable register 
+# Interrupt enable/mask bits for CPU ienable register
 # ---------------------------------------------------
 .equ		IRQ_UART_MASK,			0b00001
 .equ		IRQ_TIMER_MASK,			0b00010
 .equ		IRQ_PUSHBUTTON_MASK,	0b00100
 .equ		IRQ_ACCEL_MASK,			0b01000
 .equ		IRQ_DMA_MASK,			0b10000
-
-
-
-
-
--- HW4/samuelCuthbertson_irq_homework4.s
+++ HW4/samuelCuthbertson_irq_homework4.s
@@ -1,18 +1,13 @@
-# This is a bank template to use for your homework assignments
-
 # --------------------------------------------------------------
-#      Author: <fill in your name>
-#  Assignment: homeworkN  <change N to the appropriate value>
-#        Date: <fill in the date you complete your code>
+#      Author: Samuel Cuthbertson
+#  Assignment: homework4  <change N to the appropriate value>
+#        Date: 11/5/2017
 #      System: DE10-Lite PDS Computer
-# Description: <write a short description of what your code does>
+# Description: Counts using interrupts while also displaying a night-rider
+#								slide on the LEDS. Displays count in decimal 0-99 on HEX0/1,
+#								logs count rolling over to UART.
+# Attribution: Some pieces copied verbatim from class examples.
 # --------------------------------------------------------------
-
-# --------------------------------------------------------------------------------------
-# This template is for developing code that implements interrupt driven IO.
-# --------------------------------------------------------------------------------------
-
-
 
 		# ---------------------------------------------------------
 		# RESET SECTION
@@ -55,7 +50,7 @@ exception_handler:
 		.text
 
 		.include 	"address_map_nios2.s"
-    .equ		DELAY_VALUE, 200000
+    .equ		DELAY_VALUE, 75000
 		.global 	_start
 
 _start:
@@ -93,12 +88,18 @@ _start:
 		# ---------------------------------------------------
 		# enable timer to create an IRQ to the CPU
     movia       r15, TIMER_BASE
-		movia 			r7, time_delay
+
+		movia 			r7, time_delayl
 		ldw         r6, (r7)
-    stwio       r6, 0x20(r15) # Set the period 1 reg
-    movia       r6, 0b111
-    stwio       r6, (r15) # Set the status to 1
-    stwio       r6, 0x10(r15) # Set the ITO, CONT, and START to 1
+   	sthio       r6, 0x8(r15) # Set the periodl reg
+
+		movia 			r7, time_delayh
+		ldw         r6, (r7)
+   	sthio       r6, 0xC(r15) # Set the periodh reg
+
+		movia       r6, 0b0101
+    stbio       r6, 0x4(r15) # Set the status to 101: start, do cont, do IQR
+
 
 		# ---------------------------------------------------
 		# Configure CPU to take external hardware interrupts
@@ -111,6 +112,22 @@ _start:
 		# Set status[PIE] to enable the CPU
 		movi				r7, 0b0001
 		wrctl				status, r7
+
+		/* ----------------------------------------------------------
+		 * Finish initialization
+		---------------------------------------------------------- */
+
+		/* Display the lower 4-bits of the count value on HEX0 */
+		/* We "cheat" because we know its assembled to 0. See count: .word below */
+		movia		   r4, segement_table 	/* r4 -> segment table base */
+		mov  		   r5, r0               /* initial count value */
+		add			   r4, r4, r5          /* point (offset) to char code in table */
+		ldbu		   r5, (r4)            /* get the char code from the table */
+		slli			 r7, r5, 0x8 				 # Use that for the upper digit
+		ldbu		   r5, (r4)            /* get the char code from the table */
+		add 			 r5, r7, r5					 # Add both digits, store in r5
+		movia		   r4, SEGA_OUT_BASE   /* r4 -> HEX0 base */
+		stwio		   r5, (r4)            /* write the value to HEX0 */
 
 
 
@@ -156,32 +173,34 @@ slide_down:
 
     br      loop
 
-
-		# ------------------------------------------------------------
-		# print ()
-		# Takes as argument string pointed to by r4, prints to UART.
-		#
-print:
-		ldwio		r18, 4(r16)         # Is there space available in the TX FIFO?
-		andhi   r18, r18, 0xFFFF    # Only look at the upper 16-bits.
-		beq			r18, r0, print 		  # No space, wait for space to become available.
-
-		# OK, there is space in the TX FIFO, send the character to the host
-		ldb			r19, (r4)							# Load our character to be sent
-		beq 		r0, r19, _print_ret		# If null, return. End of string
-		stbio		r19, (r16)						# Else, send character
-
-		# Iterate
-		addi		r4, r4, 0x1
-		br			print
-
-_print_ret:
-		ret
-
 # --------------------------------------------------------------
 # End of main program
 # --------------------------------------------------------------
 
+# ------------------------------------------------------------
+# print ()
+# Takes as argument string pointed to by r4, prints to UART.
+#
+print:
+				movia 			r16, UART_BASE
+				ldwio				r17, 4(r16)         # Is there space available in the TX FIFO?
+				andhi   		r17, r17, 0xFFFF    # Only look at the upper 16-bits.
+				beq					r17, r0, print 		  # No space, wait for space to become available.
+
+				# OK, there is space in the TX FIFO, send the character to the host
+				ldb					r17, (r4)							# Load our character to be sent
+
+				beq 				r0, r17, _print_ret		# If null, return. End of string
+
+
+				stbio				r17, (r16)						# Else, send character
+
+				# Iterate
+				addi				r4, r4, 0x1
+				br					print
+
+_print_ret:
+				ret
 
 
 
@@ -192,7 +211,7 @@ _print_ret:
 interrupt_service_routine:
 
 		# Adjust the size of the stack frame as needed by your code
-		.equ		ISR_STACKSIZE,	6*4 		# 5 32-bit words
+		.equ		ISR_STACKSIZE,	9*4 		# 7 32-bit words
 
         # make a stack frame
         subi        sp, sp, ISR_STACKSIZE
@@ -213,36 +232,93 @@ interrupt_service_routine:
 skip_ea_dec:
         stw         ea,  4(sp)		# save the exception address
         stw         ra,  8(sp)		# save the current subrountine's ra
-        stw         r11,  12(sp)		# save the registers we use in this routine
-        stw         r12,  16(sp)
-        stw         r8,  20(sp)
+        stw         r4,  12(sp)		# save the registers we use in this routine
+        stw         r5,  16(sp)
+        stw         r6,  20(sp)
+        stw         r7,  24(sp)
+
+        stw         r16,  28(sp)  # used by print
+        stw         r17,  32(sp)
 
 		# bail if IRQ is not external hardware interrupt
         beq         et, r0, end_isr     # interrupt is not external IRQ
 
-		# Determine source of the interrupt
-
-				movia		r4, counter_str   # Call print on our string
-				call 		print
+		# Determine source of the interrupt - can only be the timer in this example
 
 		# Service the interrupting device
+		/* update the count in memory         */
+				movia       r4, count           # r4 -> mmory location that holds the count variable
+				ldw         r5, (r4)            # fetch the count from ememory
+				addi        r5, r5, 0x1      		# increment the count
+
+				andi				r6, r5, 0x0F				# See if lower count is 10
+				movia				r7, 0xA
+				bne					r7, r6, do_display  # If it's not, just update display
+
+
+				movia       r4, count						# First Digit is 10
+				ldw         r5, (r4)            # fetch the count from ememory
+				andi				r6, r5, 0xF0				# Update higher count
+				srli				r6, r6, 0x4
+				addi				r6, r6, 0x1					# increment
+				slli				r5, r6, 0x4
+				movia				r7, 0xA							# See if upper digit is 10
+				bne					r7, r6, do_display
+
+				# They're both 10! Roll over
+				movia				r5, 0x00
+				movia				r4, counter_str
+				call 				print
+
+do_display:
+				movia       r4, count           # r4 -> mmory location that holds the count variable
+				stw         r5, (r4)            # store the count back to memory
+
+		/* display the lower 4-bits of the 32-bit count value on HEX0 */
+				movia				r4, segement_table 	/* r4 -> segment table base */
+				andi				r5, r5, 0x000F
+				add					r4, r4, r5          /* r4 -> point (offset) to char code in table */
+				ldbu				r5, (r4)            /* get the char code from the table */
+				# Let's save the lower bits in r7
+				mov 				r7, r5
+
+		/* display the lower 4-bits of the 32-bit count value on HEX0 */
+				movia       r4, count           # r4 -> mmory location that holds the count variable
+				ldw         r5, (r4)            # fetch the count from ememory
+				movia				r4, segement_table 	/* r4 -> segment table base */
+				andi				r5, r5, 0x00F0
+				srli				r5, r5, 0x4
+				add					r4, r4, r5          /* r4 -> point (offset) to char code in table */
+				ldbu				r5, (r4)            /* get the char code from the table */
+
+				slli				r5, r5, 0x8					# put upper digit in place
+				add					r5, r5, r7					# combine digits
+
+				movia				r4, SEGA_OUT_BASE   /* r4 -> HEX0 base */
+				stwio				r5, (r4)            /* write the HEX0 */
+
 
 		# Clear the source of the interrupt
-        movia       r15, TIMER_BASE
-        movia       r6, 0b0
-        stwio       r6, (r15)
+        movia       r5, TIMER_BASE
+        movia       r6, 0b00
+        stwio       r6, (r5)
 
+		# Restart the timer
+				movia       r6, 0b0101
+				stbio       r6, 0x4(r5) # Set the status to 101: start, do cont, do IQR
 
 end_isr:
         # restore registers we used
         ldw         et,  0(sp)
         ldw         ea,  4(sp)
         ldw         ra,  8(sp)
-        ldw         r11,  12(sp)
-        ldw         r12, 16(sp)
-				ldw					r8, 20(sp)
+        ldw         r4, 12(sp)
+        ldw         r5, 16(sp)
+				ldw					r6, 20(sp)
+				ldw         r7, 24(sp)
 
-
+				ldw         r16,  28(sp)  # used by print
+        ldw         r17,  32(sp)
 
         # free the stack frame
         addi        sp, sp, ISR_STACKSIZE
@@ -251,34 +327,47 @@ end_isr:
 		eret		# return from exception
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 		# ---------------------------------------------------------
 		# DATA SECTION
 		# ---------------------------------------------------------
 		.data
+		    				.align 		2	# align to 2^2=4 byte boundary
 
-        				.align 		2	# align to 2^2=4 byte boundary
-time_delay:  		.word   	40
-counter_str:		.asciz		"The counter has rolled over\n"
+counter_str:		.ascii		"The counter has rolled over\n\0\0\0" # Extra nulls b/c of align
+
+time_delayl:  	.word   	0x9680 # Magic value -> 1/4 sec interrupts
+time_delayh:		.word			0x0098
 
 
+# A global variable used by the interrupt service routine.
+# Main code (ie _start code) should NEVER write this location !!!
+count:  .word       0x00 /* memory location that holds the current count */
+
+
+/* ------------------------------------------------- */
+/* 7-segment display character code table */
+/* This is another example of how the assembler can be */
+/* used to build a control table in memory */
+segement_table:
+		.byte					0x3F /* 0 */
+		.byte					0x06 /* 1 */
+		.byte					0x5B /* 2 */
+		.byte					0x4F /* 3 */
+
+		.byte					0x66 /* 4 */
+		.byte		  		0x6D /* 5 */
+		.byte					0x7D /* 6 */
+		.byte					0x07 /* 7 */
+
+		.byte					0x7F /* 8 */
+		.byte					0x67 /* 9 */
+		.byte					0x77 /* A */
+		.byte					0x7C /* b */
+
+		.byte					0x39 /* C */
+		.byte					0x5E /* d */
+		.byte					0x79 /* E */
+		.byte					0x71 /* F */
 
 
 		.end		# end of assembly.
